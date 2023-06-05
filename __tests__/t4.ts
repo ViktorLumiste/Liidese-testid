32699b6f41ed34396c35d9af22a8df1846a52f64
ViktorLumiste <viktor.lumiste@gmail.com>
2023-06-05 10:38:13 +0300

    Liidese testid

--- __tests__/t4.ts
+++ __tests__/t4.ts
@@ -1,33 +1,5 @@
-import nodemailer from 'nodemailer';
-let transporter=nodemailer.createTransport({
-    service: 'gmail',
-    auth:{
-        user: 'juku@juurikas.ee',
-        pass: 'kala'
-    }
+import {initApp} from "../mailer";
+
+test('start', () => {
+    expect(initApp(200000, console.log)).toBe(true);
 });
-
-
-
-function sendLog(content: string){
-    let mailOptions={
-        from: 'juku@juurikas.ee',
-        to: 'juku@juurikas.ee',
-        subject: 'Log',
-        text: content
-    };
-    transporter.sendMail(mailOptions);
-}
-
-
-function fakeInit(){
-    console.log("Käivitus kell "+new Date());
-}
-
-export function initApp(freeMemory: number,  mailFn: any): boolean{
-    if(freeMemory<100000){mailFn("Vaba mälu ainult "+freeMemory); return false;}
-    mailFn("Rakendus käivitus");
-    return true;
-}
-
-initApp(1500000,  console.log);
